8697cc269939c9f145b3e50c271362a6fcad6a0b
Foster Curtis <foster.curtis@plaidpony.com>
2024-11-07 21:19:33 -0700

    Finished app.jsx

---
+++ src/home/home.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export function Home() {
+    return (
+        <main class="bg-light d-flex flex-column container-fluid text-center" display="flex-column">
+        <div class="row justify-content-evenly">
+            <section class="card col-5 bg-primary border border-2 border-dark" style="--bs-bg-opacity: .60;">
+                <div class="card-body">
+                    <h3 class="card-title">Test Your Password Strength!</h3>
+                    <div class="row justify-content-center">
+                        <div class="col-md-6">
+                            <input class="form-control my-3" type="text" placeholder="Your Password Here" id="passwordTester" />
+                        </div>
+                        <p>Password Strength: <em style="color:rgb(253, 232, 0)" class="fwt-bold">Good</em></p>
+                        <p>Time to Crack: 3 hours</p>
+                    </div>
+                </div>
+            </section>
+            <section class="card shadow-lg col-5 border border-secondary border-2">
+                <div class="card-body">
+                    <h3 class="card-title">Generate a Passphrase</h3>
+                    <div><button class="btn btn-primary">Generate</button></div>
+                    <div class="card-text">Memory Tip: ....</div>
+                </div>
+            </section>
+            
+        </div>
+    </main>
+    )
+}
---
+++ src/leaderboard/leaderboard.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+export function Leaderboard() {
+    return (
+        <main class="bg-light d-flex flex-column container-fluid text-center">
+                <div class="row justify-content-center">
+                    <img src="trophy-clipart.png" alt="trophy" id="trophy"/>
+                    <h1 class="text-primary mb-2">Leaderboard</h1>
+                    <h3 class="text-secondary mb-3">Generate and test your passwords to take your place on the podium!</h3>
+                    <section class="col-10">
+                        <table class="table table-striped table-hover" id="leaderboard">
+                            <thead class="table-dark">
+                                <tr>
+                                    <th>User</th>
+                                    <th>PassPhrases Generated</th>
+                                    <th>Passwords Tested</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>myUsernameIsAPassPhrase</td>
+                                    <td>239</td>
+                                    <td>120</td>
+                                </tr>
+                                <tr>
+                                    <td>CantHackMe</td>
+                                    <td>58</td>
+                                    <td>37</td>
+                                </tr>
+                                <tr>
+                                    <td>Stephen</td>
+                                    <td>19</td>
+                                    <td>40</td>
+                                </tr>
+                                <tr>
+                                    <td>pickle</td>
+                                    <td>21</td>
+                                    <td>3</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </section>
+                    <p>Rankings are calculated by <em style="color:rgb(85, 85, 85)">Passphrases Genetrated + Passwords Tested</em></p>
+                </div>
+            </main>
+    )
+}
