b2ad6598bd0a281b59c3b723176168b038c6e760
Foster Curtis <foster.curtis@plaidpony.com>
2024-11-09 21:42:26 -0700

    added newUser page

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,59 @@
 import React from 'react';
 import { BrowswerRouter, Navlink, Route, Routes } from 'react-router-dom';
 import { Login } from './login/login';
+import { NewUser } from '.newUser/newUser';
+import { Home } from '.home/home';
+import { Leaderboard } from './leaderboard/leaderboard';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-function App() {
-    ;
+export default function App() {
+    return (
+        <BrowswerRouter>
+            <header className="bg-primary container-fluid">
+                <nav className="navbar navbar-expand-sm bg-primary fixed-top">
+                    <div className="container-fluid">
+                    <a className="navbar-brand" href="homePage.html"><b>PassPhrase</b></a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="home">Home</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link active" aria-current="page" to="">Login</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="leaderboard">Leaderboard</NavLink>
+                        </li>
+                        </ul>
+                    </div>
+                    </div>
+                </nav>
+            </header>
+
+            <Routes>
+        <Route path='/' element={<Login />} exact />
+        <Route path='/newUser' element={<NewUser />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/leaderboard' element={<Leaderboard />} />
+        <Route path='*' element={<NotFound />} />
+            </Routes>
+        
+            <footer className="bg-dark text-white-50 fixed-bottom p-3">
+            <div className="container-fluid">
+              <div className="row">
+                <div className="col">
+                  <span className="text-reset">Author: Foster Curtis</span>
+                </div>
+                <div className="col text-end">
+                  <a className="text-reset" href="https://github.com/foster-curtis/startup/tree/main">Source</a>
+                </div>
+              </div>
+            </div>
+        </footer>
+        </BrowswerRouter>
+    )
 }
---
+++ src/newUser/newUser.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export function NewUser() {
+    return (
+        <main class="bg-light d-flex flex-column container-fluid text-center">
+            <form method="get" action="homePage.html">
+                <legend class="mb-5"><h1>Create New User</h1></legend>
+                <div class="row justify-content-center">
+                    <div class="col-6">
+                        <div class="input-group row mb-3">
+                            <span class="col-3 input-group-text">Email</span>
+                            <input class="col-9 form-control" type="email" placeholder="email@example.com" />
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-6">
+                        <div class="input-group row mb-3">
+                            <span class="col-3 input-group-text">Password</span>
+                            <input class="col-9 form-control" type="password" placeholder="Passwords here" />
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-6">
+                        <div class="input-group row mb-3">
+                            <span class="col-3 input-group-text">Confirm Password</span>
+                            <input class="col-9 form-control" type="password" placeholder="Don't mess up" />
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <button class="btn btn-primary" type="submit">Create Account</button>
+                </div>
+                <div class="m-4">Logging in to PassPhrase will count each password you generate toward your place on the leaderboard!</div>
+            </form>
+        </main>
+    )
+}
